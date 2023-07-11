c766298206689a7e9805502eacc517fae84836bb
ynot01 <ynot000001@gmail.com>
2023-07-10 20:36:32 -0400

    init commit

---
+++ main.gd
@@ -0,0 +1,14 @@
+extends Node
+
+func _ready():
+	var my_dictionary:Dictionary = {
+			"Array":[1,2,3], 
+			"Dict":{"key":"value"},
+			"Hello!":2, 
+		}
+	
+	var my_yaml:String = YAML.to_string(my_dictionary)
+	var parsed_yaml:Dictionary = YAML.parse(my_yaml)
+	print(my_yaml)
+	print(my_dictionary)
+	print(parsed_yaml)
